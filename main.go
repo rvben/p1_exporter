ee0661b38a3410869e67aaab6c9c04146bb47b7d
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2020-12-29 14:14:17 +0100

    Service with variables

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -52,6 +53,10 @@ var (
 	gasConsumed = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "gas_consumed",
 		Help: "The total gas consumed in m3",
+	})
+	framesHandled = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "frames_handled",
+		Help: "The amount of serial frames handled",
 	})
 )
 
@@ -117,48 +122,77 @@ func (p *P1SerialReader) Read() string {
 	return sb.String()
 }
 
-func setMetric(match []string) {
+func setMetrics(data string) {
+	pat := regexp.MustCompile(`([0-1]\-\d:\d+.\d+.\d+).*\((.+?)(\*.*?)?\)`)
 	toSkip := []string{"0-0:96.1.1", "0-1:96.1.0", "0-1:24.1.0", "1-3:0.2.8", "0-0:1.0.0", "1-0:99.97.0", "0-0:96.13.0"}
-	if match != nil && !sliceContains(toSkip, match[1]) {
-		f, err := strconv.ParseFloat(match[2], 64)
-		if err != nil {
-			log.Printf("Could not parse (%s).\n Error: %s\n", match[0], err)
-		}
-		switch key := match[1]; key {
-		case "1-0:1.8.1":
-			powerConsumed.WithLabelValues("1").Set(f)
-		case "1-0:1.8.2":
-			powerConsumed.WithLabelValues("2").Set(f)
-		case "1-0:2.8.1":
-			powerDelivered.WithLabelValues("1").Set(f)
-		case "1-0:2.8.2":
-			powerDelivered.WithLabelValues("2").Set(f)
-		case "0-0:96.14.0":
-			currentTariff.Set(f)
-		case "1-0:1.7.0":
-			currentImport.Set(f)
-		case "1-0:2.7.0":
-			currentExport.Set(f)
-		case "0-1:24.3.0":
-			gasConsumed.Set(f)
-		case "0-1:24.2.1":
-			gasConsumed.Set(f)
-		default:
-			// log.Printf("Could not find key (%s) for value (%s).\n", match[1], match[2])
+	matches := pat.FindAllStringSubmatch(data, -1)
+	for _, match := range matches {
+		if match != nil && !sliceContains(toSkip, match[1]) {
+			f, err := strconv.ParseFloat(match[2], 64)
+			if err != nil {
+				log.Printf("Could not parse (%s).\n Error: %s\n", match[0], err)
+			}
+			switch key := match[1]; key {
+			case "1-0:1.8.1":
+				powerConsumed.WithLabelValues("1").Set(f)
+			case "1-0:1.8.2":
+				powerConsumed.WithLabelValues("2").Set(f)
+			case "1-0:2.8.1":
+				powerDelivered.WithLabelValues("1").Set(f)
+			case "1-0:2.8.2":
+				powerDelivered.WithLabelValues("2").Set(f)
+			case "0-0:96.14.0":
+				currentTariff.Set(f)
+			case "1-0:1.7.0":
+				currentImport.Set(f)
+			case "1-0:2.7.0":
+				currentExport.Set(f)
+			case "0-1:24.3.0":
+				gasConsumed.Set(f)
+			case "0-1:24.2.1":
+				gasConsumed.Set(f)
+			}
 		}
 	}
+	framesHandled.Inc()
 }
 
 func recordMetrics(scanner *bufio.Scanner) {
-	pat := regexp.MustCompile(`([0-1]\-\d:\d+.\d+.\d+).*\((.+?)(\*.*?)?\)`)
+	var sb strings.Builder
+	startR := regexp.MustCompile(`^\/`)
+	endR := regexp.MustCompile(`^!`)
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		match := pat.FindStringSubmatch(line)
-		go setMetric(match)
+		if startR.MatchString(line) {
+			break
+		}
+	}
+	for scanner.Scan() {
+		line := scanner.Text()
+		sb.WriteString(line)
+		if !strings.Contains(line, "0-1:24.3.0") {
+			sb.WriteString("\n")
+		}
+		if endR.MatchString(line) {
+			setMetrics(sb.String())
+			sb.Reset()
+		}
 	}
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func main() {
+	SerialName := getEnv("P1_SERIAL_NAME", "/dev/ttyUSB0")
+	SerialBaud := getEnv("P1_SERIAL_BAUD", "115200")
+	SerialParity := getEnv("P1_SERIAL_PARITY", "0")
+	SerialSize := getEnv("P1_SERIAL_SIZE", "8")
 
 	registry := prometheus.NewRegistry()
 	registerer := prometheus.WrapRegistererWithPrefix("p1_", registry)
@@ -169,7 +203,18 @@ func main() {
 	registerer.MustRegister(currentExport)
 	registerer.MustRegister(gasConsumed)
 
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+	parity := serial.ParityNone
+	switch SerialParity {
+	case "EVEN":
+		parity = serial.ParityEven
+	case "ODD":
+		parity = serial.ParityOdd
+	}
+
+	baud, err := strconv.Atoi(SerialBaud)
+	size, err := strconv.Atoi(SerialSize)
+
+	c := &serial.Config{Name: SerialName, Baud: baud, Parity: parity, Size: byte(size)}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
